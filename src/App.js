774e009b140c4baae044f3ba10898c89ab0a6bc6
Adam Age <Adamwage@hotmail.com>
2019-05-08 10:48:45 -0500

    Removed unused files. Added React Helmet to handle meta/title tags.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Helmet from 'react-helmet'
+import './App.css'
 
 function App() {
   return (
     <div className="App">
+      <Helmet title="Ignited Local Builds" />
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className="test">
+          Ignited Local Deving
+        </h1>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
