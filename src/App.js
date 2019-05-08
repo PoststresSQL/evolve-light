dfef694e0b426e1b74bdd3dbcb77eaf0b4f33efb
Adam Age <Adamwage@hotmail.com>
2019-05-08 12:00:10 -0500

    Added Prettier to format the code on save for uniform coding no matter who codes.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import {BrowserRouter as Router, Route} from 'react-router-dom'
 import Helmet from 'react-helmet'
 import Nav from './components/Nav'
 import NavLink from './components/NavLink'
@@ -9,38 +9,35 @@ import './App.css'
 
 // Define routes(pages)
 const routes = [
-  {
-    title: 'Home',
-    path: '/',
-    component: Home,
-    exact: true
-  }, {
-    title: 'About',
-    path: '/about',
-    component: About
-  }
+	{
+		title: 'Home',
+		path: '/',
+		component: Home,
+		exact: true
+	},
+	{
+		title: 'About',
+		path: '/about',
+		component: About
+	}
 ]
 
 function App() {
-  return (
-    <div className="App">
-      <Router>
-        <Helmet title="Ignited Local Builds" />
-        <Nav>
-          {
-            routes.map((route, i) => {
-              return <NavLink key={i} {...route} />
-            })
-          }
-        </Nav>
-        {
-          routes.map((route, i) => {
-            return <Route key={i} {...route} />
-          })
-        }
-      </Router>
-    </div>
-  )
+	return (
+		<div className='App'>
+			<Router>
+				<Helmet title='Ignited Local Builds' />
+				<Nav>
+					{routes.map((route, i) => {
+						return <NavLink key={i} {...route} />
+					})}
+				</Nav>
+				{routes.map((route, i) => {
+					return <Route key={i} {...route} />
+				})}
+			</Router>
+		</div>
+	)
 }
 
 export default App
