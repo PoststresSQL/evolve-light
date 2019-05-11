282427eb2ad29d427e11260103d71b020cd019dc
Adam Age <Adamwage@hotmail.com>
2019-05-11 13:28:38 -0500

    Renamed Nav component to NavBar for better naming convention.

---
+++ src/components/NavBar.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function NavBar() {
+	return (
+		<div className='nav-container'>
+			<div className='bar bar--sm visible-xs' />
+			<nav id='menu1' className='bar bar-1 hidden-xs'>
+				<div className='container'>
+					<div className='row'>
+						<div className='col-md-2 col-sm-3 hidden-xs'>
+							<div className='bar__module'>
+								<Link to='/'>
+									<img
+										src='https://placehold.it/150x80'
+										alt='logo'
+										className='logo logo-dark'
+									/>
+									<img
+										src='https://placehold.it/150x80'
+										alt='logo'
+										className='logo logo-light'
+									/>
+								</Link>
+							</div>
+						</div>
+
+						<div className='col-md-10 col-sm-9 text-right text-left-sm text-left xs'>
+							<div className='bar__module'>
+								<ul className='menu-horizontal text-left'>
+									{/* nav routes go here */}
+								</ul>
+							</div>
+						</div>
+					</div>
+				</div>
+			</nav>
+		</div>
+	)
+}
+
+export default NavBar
