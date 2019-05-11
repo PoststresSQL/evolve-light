b49d9cac2f0a8dd720566db3d1fcb8bef7c36ee3
Adam Age <Adamwage@hotmail.com>
2019-05-11 16:08:06 -0500

    Mocked some routes with subroutes to make navs and routes.

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Helmet from 'react-helmet'
 import NavBar from './components/NavBar'
 import Home from './views/Home'
-import SomeView from './views/SomeView'
+import Dropdown1 from './views/Dropdown1'
 import About from './views/About'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import './theme.css'
 import './App.css'
 
 // Define routes(pages)
@@ -23,22 +24,22 @@ const routes = [
 	},
 	{
 		title: 'Dropdown Link',
-		path: '/dropdown',
-		routes: [
+		path: '/dropdown/:service',
+		subroutes: [
 			{
 				title: 'Dropdown 1',
-				path: '/dropdown-1',
-				component: SomeView
+				path: '/dropdown/dropdown-1',
+				component: Dropdown1
 			},
 			{
 				title: 'Dropdown 2',
-				path: '/dropdown-2',
-				component: SomeView
+				path: '/dropdown/dropdown-2',
+				component: Dropdown1
 			},
 			{
 				title: 'Dropdown 3',
-				path: '/dropdown-3',
-				component: SomeView
+				path: '/dropdown/dropdown-3',
+				component: Dropdown1
 			}
 		]
 	}
@@ -49,9 +50,15 @@ function App() {
 		<div className='App'>
 			<Router>
 				<Helmet title='Ignited Local Builds' />
-				<NavBar />
+				<NavBar routes={routes} />
 				{routes.map((route, i) => {
-					return <Route key={i} {...route} />
+					if (route.hasOwnProperty('component')) {
+						return <Route key={i} {...route} />
+					} else {
+						return route.subroutes.map((subroute, j) => {
+							return <Route key={j} {...subroute} />
+						})
+					}
 				})}
 			</Router>
 		</div>
