99fcbafc549380c51253e80133cb2eef5e8eeb79
Adam Age <Adamwage@hotmail.com>
2019-05-12 20:20:04 -0500

    Created dummy Views for Dropdown item 2 and 3.

---
+++ src/views/Dropdown3.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+function DropDown3(props) {
+	console.log(props)
+	return (
+		<div className='main-container'>
+			<section className='text-center imagebg' data-overlay='4'>
+				<div className='background-image-holder'>
+					<img
+						alt='background'
+						src='https://placehold.it/1920x1080'
+					/>
+				</div>
+				<div className='container'>
+					<div className='row'>
+						<div className='col-sm-10 col-md-8'>
+							<h1>This is the view of Dropdown 3</h1>
+							<p className='lead'>
+								Lorem ipsum dolor sit amet consectetur
+								adipisicing elit. Reiciendis ratione, esse
+								recusandae laboriosam, eum quisquam vero, quo
+								qui velit ex aut quasi laborum quibusdam
+								voluptas illo. Veritatis eos tempore aspernatur?
+							</p>
+						</div>
+					</div>
+				</div>
+			</section>
+		</div>
+	)
+}
+
+export default DropDown3
