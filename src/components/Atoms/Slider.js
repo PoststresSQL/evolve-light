ad78112718132a9bcf758864c9f57b709d825192
Adam Age <Adamwage@hotmail.com>
2019-05-24 13:55:03 -0500

    Using Flickity to create a slider by mapping through an images array and rendering a SliderItem.

---
+++ src/components/Atoms/Slider.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import Flickity from 'flickity'
+import 'flickity/dist/flickity.min.css'
+import SliderItem from './SliderItem'
+
+class Slider extends React.Component {
+	constructor(props) {
+		super(props)
+
+		this.state = {
+			flickityReady: false
+		}
+	}
+
+	componentDidMount() {
+		const flkty = new Flickity('.main-carousel', {
+			autoPlay: 1500,
+			wrapAround: true,
+			contain: true,
+			prevNextButtons: false
+		})
+
+		flkty.playPlayer()
+	}
+
+	render() {
+		return (
+			<div class='main-carousel'>
+				{this.props.images.map((image, i) => {
+					return <SliderItem image={image} key={i} />
+				})}
+			</div>
+		)
+	}
+}
+
+export default Slider
