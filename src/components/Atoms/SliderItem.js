d051ac7ba9f72218277e97b5c63a55a280298f1e
Adam Age <Adamwage@hotmail.com>
2019-05-24 13:55:18 -0500

    Component for each image in the Slider component.

---
+++ src/components/Atoms/SliderItem.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function SliderItem() {
+	return (
+		<div class='carousel-cell'>
+			<img src='https://placehold.it/400x300' alt='placeholder' />
+		</div>
+	)
+}
+
+export default SliderItem
