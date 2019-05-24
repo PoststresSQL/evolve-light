8790bc6eade1d127cd195a5f67bb2708dd7aa4e6
Adam Age <Adamwage@hotmail.com>
2019-05-23 22:37:40 -0500

    Recreated Stack component for accordion image 1.

--- src/components/Accordions/AccordionImage1.js
+++ src/components/Accordions/AccordionImage1.js
@@ -1,10 +1,26 @@
 import React from 'react'
 
-function AccordionImage1() {
+function AccordionImage1({ image, switched, children }) {
 	return (
-		<div>
-			{/* This is a AccordionImage1 file, rename function name and export to component name */}
-		</div>
+		<section
+			className={
+				switched ? 'switchable switchable--switch' : 'switchable'
+			}>
+			<div className='container'>
+				<div className='row'>
+					<div className='col-sm-6 switchable__text'>
+						<ul className='accordion accordion-1'>{children}</ul>
+					</div>
+					<div className='col-md-5 col-sm-6'>
+						<img
+							alt='placeholder'
+							className='border--round box-shadow-wide'
+							src={image}
+						/>
+					</div>
+				</div>
+			</div>
+		</section>
 	)
 }
 
