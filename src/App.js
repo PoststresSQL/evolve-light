1969bceca97a7074383e4131ab69ad8ce4a8325b
Adam Age <Adamwage@hotmail.com>
2019-05-23 21:42:53 -0500

    Moved components into explicit component dirs.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { routes } from './routes'
 import Helmet from 'react-helmet'
-import NavBar from './components/NavBar'
+import NavBar from './components/Navigations/NavBar'
 import Footer from './components/Footer'
 // Stylesheets
 import 'bootstrap/dist/css/bootstrap.min.css'
---
+++ src/components/Accordions/AccordionImage1.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function AccordionImage1() {
+	return (
+		<div>
+			{/* This is a AccordionImage1 file, rename function name and export to component name */}
+		</div>
+	)
+}
+
+export default AccordionImage1
