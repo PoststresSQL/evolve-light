00113cf6879d7aad9a90feebf4585456fbfc9d46
Adam Age <Adamwage@hotmail.com>
2019-05-23 23:19:01 -0500

    Recreated Accordion type 2 components from Stack.

---
+++ src/components/Accordions/AccordionImage2.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+function AccordionImage2({ image, switched, children }) {
+	return (
+		<section
+			className={
+				switched ? 'switchable switchable--switch' : 'switchable'
+			}>
+			<div className='container'>
+				<div className='row'>
+					<div className='col-sm-6 switchable__text'>
+						<ul className='accordion accordion-2'>{children}</ul>
+					</div>
+					<div className='col-md-5 col-sm-6'>
+						<img
+							alt='placeholder'
+							className='border--round box-shadow-wide'
+							src={image}
+						/>
+					</div>
+				</div>
+			</div>
+		</section>
+	)
+}
+
+export default AccordionImage2
