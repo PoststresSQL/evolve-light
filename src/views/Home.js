f00fe89b7955b88850d3d2c7f696c74a53c3730e
Adam Age <Adamwage@hotmail.com>
2019-06-30 12:45:41 -0500

    Updated buttons and classes from old bootstrap.

--- src/views/Home.js
+++ src/views/Home.js
@@ -5,23 +5,22 @@ import Section from '../components/Section'
 function Home() {
 	return (
 		<div className='main-container'>
-			<Section id='home-hero' className='height-80'>
-				<div className='container pos-vertical-center'>
-					<div className='row'>
-						<div className='col-sm-10 col-md-8'>
-							<h1>Home View</h1>
-							<p className='lead'>
-								Lorem ipsum dolor sit amet consectetur
-								adipisicing elit. Nam voluptatum sequi
-								distinctio magnam temporibus laborum facilis
-								itaque possimus assumenda aliquam!
-							</p>
-							<Button pathName='/' primary>
-								Primary Button
-							</Button>
-							<Button pathName='/'>Button</Button>
-						</div>
-					</div>
+			<Section id='home-hero' className='height-60 align-self-center'>
+				<div className='col-sm-10 col-md-8'>
+					<h1>Home View</h1>
+					<p className=''>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit.
+						Nam voluptatum sequi distinctio magnam temporibus
+						laborum facilis itaque possimus assumenda aliquam!
+					</p>
+					<Button
+						pathName='#'
+						className='btn btn--primary box-shadow'>
+						Primary Button
+					</Button>
+					<Button pathName='#' className='btn box-shadow'>
+						Button
+					</Button>
 				</div>
 			</Section>
 		</div>
