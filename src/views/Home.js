447efd09369a7366782425987b554a82d084d677
Adam Age <Adamwage@hotmail.com>
2019-07-01 09:03:50 -0500

    Replaced home template text with company tag for CMS.

--- src/views/Home.js
+++ src/views/Home.js
@@ -10,20 +10,12 @@ function Home() {
 		<div className='main-container'>
 			<Section id='home-hero' className='height-60 align-self-center'>
 				<div className='col-sm-10 col-md-8'>
-					<h1>Home View</h1>
+					<h1 className='big extra-bold'>((company))</h1>
 					<p className=''>
 						Lorem ipsum dolor sit amet consectetur adipisicing elit.
 						Nam voluptatum sequi distinctio magnam temporibus
 						laborum facilis itaque possimus assumenda aliquam!
 					</p>
-					<Button
-						pathName='#'
-						className='btn btn--primary box-shadow'>
-						Primary Button
-					</Button>
-					<Button pathName='#' className='btn box-shadow'>
-						Button
-					</Button>
 				</div>
 			</Section>
 
