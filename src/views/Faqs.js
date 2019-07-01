832e5f420f57b3a2719501f7035ae542ea1259b1
Adam Age <Adamwage@hotmail.com>
2019-07-01 09:03:31 -0500

    FAQs and Process copied from Evolve v1 build; Light mode instead of Dark.

--- src/views/Faqs.js
+++ src/views/Faqs.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import Section from '../components/Section'
+import AccordionItem from '../components/Atoms/AccordionItem'
+import { Link } from 'react-router-dom'
 
 function Template() {
 	return (
@@ -8,15 +10,262 @@ function Template() {
 				<div className='container'>
 					<div className='row'>
 						<div className='col-sm-8 col-md-7'>
-							<h1>Template</h1>
-							<p className='lead'>
-								Lorem ipsum dolor sit amet consectetur
-								adipisicing elit. Maxime dolorum suscipit
-								asperiores placeat accusantium rerum animi
-								consequuntur sed optio inventore!
-							</p>
+							<h1 className='big extra-bold'>
+								Frequently Asked Questions
+							</h1>
+							<hr />
 						</div>
 					</div>
+				</div>
+			</Section>
+
+			<Section id='faqs__section' className=''>
+				<div className='col-sm-6'>
+					<ul className='accordion accordion-2'>
+						<AccordionItem title='What is Microblading?' active>
+							<p>
+								Microblading, also known as eyebrow embroidery
+								or 3D brows, is a relatively new manual method
+								for enhancing eyebrows. It is considered to be
+								permanent makeup, as compared to the traditional
+								hair stroke technique done by machine. It is
+								done using a very fine blade to deposit pigment
+								into the epidermis. Because the color is closer
+								to the surface, the strokes appear crisp and
+								very fine. There is no spilling under the skin.
+							</p>
+							<p>
+								The difference with this and ordinary permanent
+								makeup is that microblading is not done by a
+								machine. The pigment is manually placed into the
+								skin. The microblading manual pen is equipped
+								with sterile packaged blades. The blade glides
+								with gentle pressure over the skin and causes
+								fine cuts, as fine as a paper cut, leaving a
+								trace that can hardly be distinguished from real
+								hair. Depending on the drawing technique – of
+								which we are experts – it creates an individual
+								look perfect for your face and the style you are
+								trying to achieve. Above all 3D, or HD, eyebrows
+								absolutely do not appear as a tattoo.
+							</p>
+							<p>
+								Microblading is ideal for anyone wanting to
+								enhance the look of their eyebrows. The results
+								are natural looking hair-like strokes,
+								regardless of the amount of the hair you
+								currently have. People suffering from alopecia,
+								trigonometrical or any other condition that
+								causes hair loss benefit greatly from
+								microblading, as well as people who simply
+								aren’t happy with their eyebrows.
+							</p>
+						</AccordionItem>
+						<AccordionItem title='How long does it take to heal?'>
+							<p>
+								The eyebrow area will be slightly red after the
+								procedure and color will appear very dark. This
+								is normal for the procedure and part of the
+								healing process. The color will fade by up to
+								40% as it heals. In the following days, the
+								eyebrows will continue to be dark and you will
+								notice slight scabbing. Do not pick at the scabs
+								as this may damage the skin and the outcome of
+								the microbladed area. The eyebrows will lighten
+								up in color as it heals. No down time is
+								required after microblading procedure. It takes
+								approximately 7-14 days for the brows to heal
+								over, and about 1 month for the color to fully
+								set in. You'll notice the color is very light
+								right after the two weeks after healing; the
+								skin will have an opaque milky looking quality
+								to it. This is known as the milk skin and is
+								common in tattooing. This is due to the the
+								tissue still healing from the inside out even
+								though the surface of the skin is healed. True
+								color will reveal itself once healed.
+							</p>
+						</AccordionItem>
+						<AccordionItem title='Can I get my touch up session sooner than  weeks?'>
+							<p>
+								Touch up sessions should not be done earlier
+								than 4 weeks after initial microblading session
+								as pigment color has not fully settled into the
+								skin. Most importantly, this is to prevent scar
+								damage as the tissue requires time to completely
+								heal.IS IT PAINFUL?The pain level for the
+								procedure is considered mild to none. Prior to
+								the procedure your brow technician will apply a
+								topical anesthetic to numb the area, and also
+								reapplied during the procedure. Everyone
+								responds differently to anesthetic; depending on
+								the client, the feeling of the procedure has
+								been described as light cat scratches to not
+								feeling anything. Topical anesthetics used may
+								contain lidocaine, prilocaine, benzocaine,
+								tetracaine and epinephrine. After the procedure,
+								redness and slight swelling in the area is
+								normal. Clients describe the area feeling
+								similar to a light sunburn. Clients can take
+								Tylenol but not aspirin or ibuprofen.
+							</p>
+						</AccordionItem>
+						<AccordionItem title='Who is NOT suitable for the procedure?'>
+							<p>
+								Microblading is not suitable for the following
+								candidates:· Using Accutane· Have heart
+								conditions or using heart medications· Have
+								diabetes (will require a physician's note)· Have
+								skin disease or irritations · Have had Botox in
+								the last 2 weeks· Have gotten a deep tan or
+								chemical peels in the last 2 weeks· Allergic to
+								lidocaine, prilocaine, benzocaine, tetracaine or
+								epinephrine· Anyone who has any serious medical
+								conditions may require a physician's note. ·
+								Pregnant or nursing clients who want to
+								microblade can be done at your own discretion.
+								We suggest discussing with your doctor first.
+								Microblading is still considered a tattoo, and
+								just like with wine or sushi consumption, it is
+								a personal choice to do so during this period.
+							</p>
+							<h5>
+								<em>
+									* We reserve the right to refuse service to
+									anyone who we deem not suitable for this
+									procedure or for any other reason. Our brow
+									technicians make the decision to microblade
+									an individual based on their own discretion.
+								</em>
+							</h5>
+						</AccordionItem>
+						<AccordionItem title="What if I don't like it?">
+							<p>
+								As with all cosmetic procedures, individuals
+								should not make their choices lightly. We always
+								recommend clients do their research, understand
+								the procedure and compare different artists'
+								work and portfolio when making their decisions.
+								Our brow technicians will do their utmost to
+								shape and style the eyebrows to the clients
+								satisfaction prior to the procedure. Every
+								effort will be made to achieve an even
+								appearance, but please realize our faces are
+								never perfectly symmetrical. Uneven appearance
+								can be fixed during the touch up session.
+								Pigment discoloration and color correction can
+								also be fixed during the the touch up session.
+								Microbladed brows will eventually fade away if
+								no additional touch ups are done.
+							</p>
+						</AccordionItem>
+						<AccordionItem title='What are your cancellation/missed appointment policies?'>
+							<p>
+								Clients are able to cancel or reschedule
+								appointments at anytime with a minimum of 48
+								hours notice prior to the appointment time. To
+								book an appointment, a non-refundable deposit of
+								$50 is required. For a more detailed list of
+								policies and associated fees. To cancel or
+								reschedule, please email or call us. We reserve
+								the right to refuse service to clients who have
+								cancellations or rescheduling of appointments 2
+								times in a row.
+							</p>
+						</AccordionItem>
+					</ul>
+				</div>
+				<div className='col-sm-6'>
+					<ul className='accordion accordion-2'>
+						<AccordionItem title='How long does it take?'>
+							<p>
+								For the first Microblading session, the entire
+								procedure should take about 2.5-3 hours. During
+								this session, the brow technician will shape and
+								style the client’s brows for client approval. We
+								then follow with pigment color selection. Once
+								the client is satisfied with the choices, we
+								begin the procedure.The client is to come in for
+								a follow up visit in order to have any minor
+								necessary touch ups. The second Microblading
+								session takes place after 4-6 weeks after the
+								initial session. This session will take
+								approximately 1.5-2 hours to complete.
+							</p>
+						</AccordionItem>
+						<AccordionItem title='Is the Touch Up session necessary?'>
+							<p>
+								Yes! A touch up session after 4-6 weeks is
+								necessary as everyone heals differently and
+								outcome will vary. Certain areas may not pick up
+								during first session and will need to be
+								refilled. Pigment color may need to be adjusted,
+								brow thickness and more strokes can be added.
+								Individuals with oily skin often require a
+								deeper shade during the touch up session as the
+								oils tend to lighten the pigment. Most
+								importantly, your touch up session is when we
+								refine your brows to ensure it looks perfect and
+								retains the color for the whole year!
+							</p>
+						</AccordionItem>
+						<AccordionItem title='How long does it last?'>
+							<p>
+								Since microblading pigments are not implanted
+								into the skin as deep as traditional tattoos,
+								the area will naturally fade overtime.
+								Microblading typically lasts 1-3 years,
+								depending on skin type (normal, oily, dry), age
+								(youthful/mature). Skincare routine will also
+								affect the lasting effect of the pigment:
+								patients who tan, uses products with AHA or
+								Glycolic Acid with fade faster as your skin will
+								resurface faster. Each individual's color
+								retention within the skin will vary. To maintain
+								the brows looking fresh, a touch up is suggested
+								every 12-18 months. Clients can let the
+								microbladed area fade over time, but please note
+								a slight shadow may remain.
+							</p>
+						</AccordionItem>
+						<AccordionItem title='What are the Pre-Care and Post-Care procedures?'>
+							<p>
+								In order to achieve the best results, it is best
+								to follow the Pre and Post-Care instructions.
+								See <Link to='/process'>How It Works</Link>
+							</p>
+						</AccordionItem>
+						<AccordionItem title='What are the Risks and Complications?'>
+							<p>
+								We take all proper precautions for a safe
+								session by using sterilized equipment, new
+								needles and in a sanitized environment. Should
+								clients follow our pre and post care
+								instructions, there should be minimal risks or
+								complications . Although allergic reactions to
+								the pigments are rare, it could be possible for
+								certain individuals. We suggest a pigment patch
+								test for individuals who have high skin
+								sensitivities or are sensitive to:· Topical
+								makeup products· Gold, silver and nickel· Hair
+								dyesPatch tests should be performed 5-7 days
+								prior.
+							</p>
+						</AccordionItem>
+					</ul>
+				</div>
+				<div className='col-12'>
+					<p>
+						<em>
+							Should you still have more questions or inquiries,
+							feel free to give us a call at{' '}
+							<a href='tel:((phone))'>(360) 386-BROW</a>
+							(2769) or email us at&ensp;
+							<a href='mailto:evolvelashandbrow@gmail.com'>
+								evolvelashandbrow@gmail.com
+							</a>
+						</em>
+					</p>
 				</div>
 			</Section>
 		</div>
--- src/views/Process.js
+++ src/views/Process.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Section from '../components/Section'
+import Button from '../components/Atoms/Button'
 
 function Template() {
 	return (
@@ -8,15 +9,420 @@ function Template() {
 				<div className='container'>
 					<div className='row'>
 						<div className='col-sm-8 col-md-7'>
-							<h1>Template</h1>
-							<p className='lead'>
-								Lorem ipsum dolor sit amet consectetur
-								adipisicing elit. Maxime dolorum suscipit
-								asperiores placeat accusantium rerum animi
-								consequuntur sed optio inventore!
-							</p>
+							<h1 className='big extra-bold'>
+								The Microblading Process
+							</h1>
 						</div>
 					</div>
+				</div>
+			</Section>
+
+			<Section id='pre-process__section' className=''>
+				<div className='col-md-8'>
+					<h2 className='big extra-bold'>Pre-Care</h2>
+					<hr />
+					<p>
+						It's simple! Here are some things to remember not to do:
+					</p>
+					<ul className='care__list'>
+						<li className='care__list-item'>
+							<p>
+								We prefer that you do not have newly plucked or
+								waxed brows at your microblading appointment, so
+								that we can see where your natural brows grow.
+								That way we can shape your brows from scratch.
+								So, the fuller your brows are at your
+								microblading appointment, the better, as we'll
+								have more to work with.
+							</p>
+						</li>
+						<li className='care__list-item'>
+							<p>
+								Do not consume alcohol or caffeine 24 hours
+								prior to your microblading procedure (these are
+								natural blood thinners and will make you bleed
+								excessively during the procedure)
+							</p>
+						</li>
+						<li className='care__list-item'>
+							<p>
+								Do not take Aspirin, Niacin, Vitamin E or
+								Advil/Ibuprofen 48 hours before your procedure -
+								Tylenol is OK.
+							</p>
+						</li>
+						<li className='care__list-item'>
+							<p>
+								Do not use Botox in the forehead and eye area 3
+								weeks before your microblading procedure
+							</p>
+						</li>
+						<li className='care__list-item'>
+							<p>
+								Do not take Fish Oil or Vitamin E supplements 1
+								week prior (natural blood thinners)
+							</p>
+						</li>
+						<li className='care__list-item'>
+							<p>
+								Discontinue Vitamin A/Retinol products 2 weeks
+								prior to treatment
+							</p>
+						</li>
+						<li className='care__list-item'>
+							<p>
+								Discontinue prescription Retin-A products 3
+								weeks prior to your procedure - always avoid
+								application onto the brow area
+							</p>
+						</li>
+						<li className='care__list-item'>
+							<p>Do not work out the day of the procedure</p>
+						</li>
+						<li className='care__list-item'>
+							<p>Do not tan 2 weeks prior</p>
+						</li>
+						<li className='care__list-item'>
+							<p>
+								Do not have any type of facial/peel or
+								electrolysis 1 week prior to treatment
+							</p>
+						</li>
+						<li className='care__list-item'>
+							<p>
+								Avoid Nutritional Shakes, "Hair, Skin, Nail"
+								supplements 48 hours prior to procedure
+							</p>
+						</li>
+						<li className='care__list-item'>
+							<p>
+								Avoid use of any Alpha Hydroxy Acid (AHA)
+								products close to the eyebrow area 2 weeks prior
+								to and 2 weeks after your procedure
+							</p>
+						</li>
+						<li className='care__list-item'>
+							<p>
+								Avoid Chemical Peels, Microdermabrasion or other
+								invasive facials 3 weeks prior to procedure
+							</p>
+						</li>
+						<li className='care__list-item'>
+							<p>
+								Please note; Your skin will be more sensitive
+								during your menstrual cycle.
+							</p>
+						</li>
+					</ul>
+					<p>
+						Please take in account that this procedure will take
+						about 2 1/5 hours depending on your brows and their
+						existing condition. Due to the nature of this service we
+						kindly ask that you arrive unaccompanied to your
+						scheduled appointment. Please leave friends,
+						boyfriends/husbands, children and pets at home.
+					</p>
+				</div>
+			</Section>
+
+			<section
+				className='text-center testimonial testimonial-1 imagebg'
+				data-overlay='6'>
+				<div
+					className='background-image-holder'
+					style={{
+						backgroundColor: 'rgba(0, 0, 0, 0)',
+						backgroundPosition: 'initial',
+						backgroundRepeat: 'no-repeat',
+						backgroundAttachment: 'scroll',
+						backgroundImage: `url("https://seedlogix.com/users/u147/147577/background-05.jpg")`,
+						backgroundSize: 'auto',
+						backgroundOrigin: 'padding-box',
+						backgroundClip: 'border-box',
+						opacity: '1'
+					}}>
+					<img
+						alt='background'
+						src='https://seedlogix.com/users/u147/147577/background-05.jpg'
+					/>
+				</div>
+				<div className='container'>
+					<div className='row justify-content-center'>
+						<div className='col-12 col-lg-8'>
+							<div className=''>
+								<blockquote>
+									Lorem ipsum dolor sit amet consectetur,
+									adipisicing elit. Temporibus iste dicta
+									aliquid repudiandae obcaecati veritatis amet
+									quae assumenda voluptatibus tempore fuga
+									provident ipsum omnis harum perferendis,
+									placeat sint perspiciatis odio.
+								</blockquote>
+								<h4 className='semi-bold'>Cameron</h4>
+							</div>
+						</div>
+						<div className='col-12 col-lg-8'>
+							<Button
+								pathName='/contact'
+								className='btn btn--primary border--round'>
+								Setup A Consultation
+							</Button>
+						</div>
+					</div>
+				</div>
+			</section>
+
+			<Section id='post-process__section'>
+				<div>
+					<h2 className='big extra-bold'>Post-Care</h2>
+					<hr />
+					<p>
+						Post-care steps are straightforward and simple. Your
+						microblading technician will provide you a free After
+						Care Kit and go over all the details of your after care
+						at your appointment time. It is absolutely crucial that
+						you follow precise Post-Care instructions to achieve the
+						full results of the Microblading procedure. If
+						directions are not followed there is a risk that your
+						procedure results won't last as intended. Your regular
+						brow maintenance of tweezing, threading, waxing and/or
+						tinting may continue as normal once your new brows have
+						healed (after 2 weeks).
+					</p>
+					<h4 className='semi-bold text--uppercase'>
+						Day 1 - Day of your microblading procedure
+					</h4>
+					<ul className='care__list'>
+						<li className='care__list-item'>
+							<p>
+								Wash your microbladed brows every 2-3 hours with
+								the cleanser provided to you in your After-Care
+								Kit. Wash your hands with soap before touching
+								the brow area, apply cleanser on to your brows,
+								rinse with water.
+							</p>
+						</li>
+						<li className='care__list-item'>
+							<p>
+								Either air dry or dab the brow area after
+								washing. Use a clean tissue and pat dry - do not
+								use a towel on the brow area.
+							</p>
+						</li>
+						<li className='care__list-item'>
+							<p>
+								Apply ointment with a Q-Tip on to your brows
+								in-between the washing (allow eyebrows to
+								completely dry before applying ointment)
+							</p>
+						</li>
+						<li className='care__list-item'>
+							<p>Apply ointment once more before bedtime</p>
+						</li>
+						<li className='care__list-item'>
+							<p>
+								Continue to apply ointment with a Q-tip onto
+								your brows 3 times per day (apply every morning,
+								mid-day and night ) for the next 7 days (10 days
+								if your brows feel dry).
+							</p>
+						</li>
+					</ul>
+					<h4 className='semi-bold text--uppercase'>Day 2-10</h4>
+					<ul className='care__list'>
+						<li className='care__list-item'>
+							<p>
+								DO NOT rub, pick or scratch the treated area.
+								Let any scabbing or dry skin naturally exfoliate
+								off. Picking can cause scarring and pigment
+								loss!
+							</p>
+						</li>
+						<li className='care__list-item'>
+							<p>
+								NO makeup or skincare products is to be used on
+								your brow area for 2 weeks.
+							</p>
+						</li>
+						<li className='care__list-item'>
+							<p>
+								Avoid heavy sweating for 1 week - hot yoga,
+								spinning class, running etc. (pigment can be
+								expelled through your pores when sweating and
+								compromise the healed result)
+							</p>
+						</li>
+						<li className='care__list-item'>
+							<p>
+								No submerging your face for 2 weeks - No
+								Swimming! Plan your vacations accordingly
+							</p>
+						</li>
+						<li className='care__list-item'>
+							<p>
+								No hot showers, pool, sauna, steam rooms and/or
+								hot baths for 3 weeks.
+							</p>
+						</li>
+						<li className='care__list-item'>
+							<p>
+								NO facials, chemical treatments and
+								microdermabrasion for 4 weeks.
+							</p>
+						</li>
+						<li className='care__list-item'>
+							<p>
+								Eyebrow tinting should not be undertaken for 2
+								weeks after your procedure.
+							</p>
+						</li>
+						<li className='care__list-item'>
+							<p>
+								Avoid direct sun exposure and tanning beds for a
+								minimum of 4 weeks after your procedure. Direct
+								sunlight can cause the pigment to change color
+								while it is healing and can also cause hyper
+								pigmentation and scarring. After 10 days, once
+								the area has healed completely, use SPF/sunblock
+								when going out in the sun to avoid fading of the
+								pigment.
+							</p>
+						</li>
+						<li className='care__list-item'>
+							<p>
+								Keep your bangs and hair pulled back from your
+								face for the first 10 days.
+							</p>
+						</li>
+						<li className='care__list-item'>
+							<p>
+								Avoid Retin-A, Chemical Peels and Microderms
+								around the brow area once healed.
+							</p>
+						</li>
+						<li className='care__list-item'>
+							<p>
+								Try avoiding sleeping on your face/brows for the
+								first 7 days.
+							</p>
+						</li>
+					</ul>
+					<h5>
+						<em>
+							During healing (first 2 weeks) your eyebrows will
+							appear darker and bolder due to natural scabbing and
+							healing. This is very common for all permanent
+							cosmetic procedures.
+						</em>
+					</h5>
+				</div>
+			</Section>
+
+			<Section className=''>
+				<div className='col-12'>
+					<h2 className='big extra-bold'>What's Next?</h2>
+					<hr />
+				</div>
+				<div className='col-md-8'>
+					<h3 className='bold'>
+						What to expect after your microblading procedure
+					</h3>
+					<p>
+						After your Microblading procedure your brows go through
+						several stages of healing. It is crucial that you follow
+						all steps and recommendations provided to you. Right
+						after your treatment your brows will appear bolder and
+						darker than your final healed result. The intensity of
+						the pigment will settle within a few days and after the
+						6th-9th day you will see what your brows will end up
+						looking like. Be patient - great things come to those
+						who wait.
+					</p>
+					<h3 className='bold'>Touch Up Appointment</h3>
+					<p>
+						A touch-up appointment is necessary for you to receive
+						the full benefits of this procedure. This touch-up
+						appointment is complimentary and should be scheduled to
+						be performed between 4-6 weeks after your initial visit.
+						Important! The touch-up appointment is crucial to
+						achieve optimal results - do not skip this step!
+					</p>
+					<h3 className='bold'>Your new eyebrows</h3>
+					<p>
+						Healed results will vary with each individual client
+						depending on skin type and other factors. But most
+						importantly - your results depend on how well you cared
+						for your new brows during the initial two weeks of
+						healing. Detailed post care instructions and how to best
+						care for your new brows will be provided to you after
+						your visit at Evolve Lash and Brow.
+					</p>
+					<h5>
+						<em>
+							*We advise new clients to book their Microblading
+							appointments 1 month in advance of vacations,
+							weddings, and other special occasions (remember,
+							there's also a Touch up appointment). This allows
+							adequate time for healing of both your initial
+							Microblading procedure as well as the Touch-Up
+							appointment.
+						</em>
+					</h5>
+				</div>
+				<div className='col-md-4'>
+					<h4 className='semi-bold'>
+						* Permanent makeup is NOT recommended for any clients
+						who are or have:
+					</h4>
+					<ul className='care__list'>
+						<li className='care__list-item'>
+							<p>Pregnant or Nursing</p>
+						</li>
+						<li className='care__list-item'>
+							<p>Allergic to anesthetic (Lidocaine)</p>
+						</li>
+						<li className='care__list-item'>
+							<p>Had Botox in the past 3 weeks</p>
+						</li>
+						<li className='care__list-item'>
+							<p>Used Accutane in the past year</p>
+						</li>
+						<li className='care__list-item'>
+							<p>Tendency towards keloids</p>
+						</li>
+						<li className='care__list-item'>
+							<p>Diabetic</p>
+						</li>
+						<li className='care__list-item'>
+							<p>Viral infections and/or diseases</p>
+						</li>
+						<li className='care__list-item'>
+							<p>Epilepsy</p>
+						</li>
+						<li className='care__list-item'>
+							<p>A Pacemaker or major heart problems</p>
+						</li>
+						<li className='care__list-item'>
+							<p>Had an Organ transplant</p>
+						</li>
+						<li className='care__list-item'>
+							<p>Seborrheic dermatitis</p>
+						</li>
+						<li className='care__list-item'>
+							<p>
+								Skin irritations or Psoriasis near the treated
+								area (rashes, sunburn, acne, etc.)
+							</p>
+						</li>
+						<li className='care__list-item'>
+							<p>
+								Sick (cold, flu, viral infection outbreak etc.)
+							</p>
+						</li>
+						<li className='care__list-item'>
+							<p>Extremely Oily or Problematic Skin</p>
+						</li>
+					</ul>
 				</div>
 			</Section>
 		</div>
