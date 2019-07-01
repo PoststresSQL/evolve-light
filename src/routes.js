5228eeec2b9b299107f573e3b69683603e2c8682
Adam Age <Adamwage@hotmail.com>
2019-07-01 07:46:37 -0500

    Created template files for all routes and added routes to routes.js file.

--- src/routes.js
+++ src/routes.js
@@ -1,35 +1,76 @@
 import Home from './views/Home'
 import About from './views/About'
-import SomeView from './views/SomeView'
+import Microblading from './views/Microblading'
+import MicroShading from './views/MicroShading'
+import MicroTouchUp from './views/MicroTouchUp'
+import MicroAnnual from './views/MicroAnnual'
+import Process from './views/Process'
+import Faqs from './views/Faqs'
+import Contact from './views/Contact'
 import Template from './views/Template'
 
 // Define routes(pages)
 export const routes = [
 	{
-		title: 'Home',
+		title: 'home',
 		path: '/',
 		component: Home,
 		exact: true
 	},
 	{
-		title: 'About',
-		path: '/about',
-		component: About
-	},
-	{
-		title: 'Dropdown Menu',
+		title: 'microblading services',
 		subroutes: [
 			{
-				title: 'Some View',
-				path: '/dropdown-menu/some-view',
-				component: SomeView
+				title: 'microblading',
+				path: '/microblading-services/microblading',
+				component: Microblading
 			},
 			{
-				title: 'Template',
-				path: '/dropdown-menu/template',
+				title: 'microblading & shading',
+				path: '/microblading-services/microblading-and-shading',
+				component: MicroShading
+			},
+			{
+				title: 'microblading touch-ups',
+				path: '/microblading-services/microblading-touch-ups',
+				component: MicroTouchUp
+			},
+			{
+				title: 'annual touch-ups',
+				path: '/microblading-services/annual-touch-ups',
+				component: MicroAnnual
+			}
+		]
+	},
+	{
+		title: 'how it works',
+		path: '/process',
+		component: Process
+	},
+	{
+		title: 'faqs',
+		path: '/faqs',
+		component: Faqs
+	},
+	{
+		title: 'our company',
+		subroutes: [
+			{
+				title: 'about terra',
+				path: '/our-company/about-terra',
+				component: About
+			},
+			{
+				title: 'blog',
+				path: '#',
 				component: Template
 			}
 		]
+	},
+	{
+		title: 'contact',
+		path: '/contact',
+		component: Contact
 	}
 ]
 
---
+++ src/views/Faqs.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import Section from '../components/Section'
+
+function Template() {
+	return (
+		<div className='main-container'>
+			<Section className='height-auto'>
+				<div className='container'>
+					<div className='row'>
+						<div className='col-sm-8 col-md-7'>
+							<h1>Template</h1>
+							<p className='lead'>
+								Lorem ipsum dolor sit amet consectetur
+								adipisicing elit. Maxime dolorum suscipit
+								asperiores placeat accusantium rerum animi
+								consequuntur sed optio inventore!
+							</p>
+						</div>
+					</div>
+				</div>
+			</Section>
+		</div>
+	)
+}
+
+export default Template
